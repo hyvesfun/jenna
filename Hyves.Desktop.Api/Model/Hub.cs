59e49a9155afa5bd71560f0bc1f8292a621d3a4f
Anuj Ahuja <anujmca@yahoo.com>
2011-09-05 20:30:14 +0200

    Added hub service to hyves api

---
+++ Hyves.Desktop.Api/Model/Hub.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hyves.Api.Model
+{
+    public class Hub
+    {
+        public string  hubid  { get; set; }
+        public string  hubvisible  { get; set; }
+        public string  hubtype  { get; set; }
+        public string  title  { get; set; }
+        public string  description  { get; set; }
+        public string  mediaid  { get; set; }
+        public string  hubcategoryid  { get; set; }
+        public string  userscount  { get; set; }
+        public string  url  { get; set; }
+        public Media  profilepicture  { get; set; }
+        public string created { get; set; }
+    }
+}
